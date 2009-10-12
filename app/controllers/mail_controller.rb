a708530577b19d8ba1620b887cbe4ebdf1dc0493
TekWiz <warlickt@operissystems.com>
2009-10-12 11:32:52 -0500

    DRY-up and fix reCAPTCHA not checking with mailer.post_to_page?

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,5 +1,5 @@
 class MailController < ApplicationController
-  include Recaptcha::Verification
+  include RecaptchaMailer
   
   no_login_required
   skip_before_filter :verify_authenticity_token  
@@ -7,36 +7,12 @@ class MailController < ApplicationController
   def create
     @page = Page.find(params[:page_id])
     @page.request, @page.response = request, response
+    mail, config = process_mail(@page, request)
 
-    config, part_page = config_and_page(@page)
-
-    mail = Mail.new(part_page, config, params[:mailer])
-    @page.last_mail = part_page.last_mail = mail
-    process_mail(mail, config)
-
-    if mail.send
+    if mail.sent?
       redirect_to (config[:redirect_to] || "#{@page.url}#mail_sent")
     else
       render :text => @page.render
     end
-  end
-  
-  private
-  
-  # Hook here to do additional things, like check a CAPTCHA
-  def process_mail(mail, config)
-    if respond_to?(:verify_recaptcha)
-      verify_recaptcha(:model => mail)
-    else
-      logger.warn "reCAPTCHA not available."
-    end
-  end
-
-  def config_and_page(page)
-    until page.part(:mailer) or (not page.parent)
-      page = page.parent
-    end
-    string = page.render_part(:mailer)
-    [(string.empty? ? {} : YAML::load(string).symbolize_keys), page]
   end
 end
--- lib/mailer_process.rb
+++ lib/mailer_process.rb
@@ -1,4 +1,6 @@
 module MailerProcess
+  include RecaptchaMailer
+  
   def self.included(base)
     base.class_eval { 
       alias_method_chain :process, :mailer 
@@ -9,13 +11,8 @@ module MailerProcess
   def process_with_mailer(request, response)
     # If configured to do so, receive and process the mailer POST
     if Radiant::Config['mailer.post_to_page?'] && request.post? && request.parameters[:mailer]
-      config, part_page = mailer_config_and_page
-
-      mail = Mail.new(part_page, config, request.parameters[:mailer])
-      self.last_mail = part_page.last_mail = mail
-      # process_mail(mail, config)
-
-      if mail.send
+      mail, config = process_mail(self, request)
+      if mail.sent?
         response.redirect(config[:redirect_to], "302 Found") and return if config[:redirect_to]
         # Clear out the data and errors so the form isn't populated, but keep the success status around.
         self.last_mail.data.delete_if { true }
@@ -24,22 +21,4 @@ module MailerProcess
     end
     process_without_mailer(request, response)
   end
-
-  private
-    def mailer_config_and_page
-      page = self
-      until page.part(:mailer) or (not page.parent)
-        page = page.parent
-      end
-      string = page.render_part(:mailer)
-      [(string.empty? ? {} : YAML::load(string).symbolize_keys), page]
-    end
-
-    def process_mail( mail, config )
-      if respond_to?(:verify_recaptcha)
-        verify_recaptcha(:model => mail)
-      else
-        logger.warn "reCAPTCHA not available."
-      end
-    end
 end
